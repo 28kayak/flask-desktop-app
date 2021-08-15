5c3e3e061f9833ff716af9110d27a553e74cffbf
kaya.ota@gmail.com <0246kaya>
2021-08-15 18:38:25 +0900

    edit file_handler.py to enable sending back variable to main.py

--- file_handler.py
+++ file_handler.py
@@ -1,7 +1,11 @@
 import csv 
 
 def calculate(csv_file):
+    #allocate data as list
+    data = []
     with open(csv_file, newline='') as f:
         reader = csv.reader(f)
         for row in reader: 
-            print(row)
+            data.append(row)
+        print(data)
+    return data
