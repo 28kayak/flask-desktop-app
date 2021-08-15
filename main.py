068e0b4e85ab365e1344fd7a4c92d364d9ebaad4
kaya.ota@gmail.com <0246kaya>
2021-08-15 15:15:08 +0900

    edit main.py to accept file upload from html

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, request
 from flask import render_template
 from flaskwebgui import FlaskUI
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+#Bootstrap(app)
+#app.config['UPLOAD_EXTENSIONS'] = ['.csv']
 ui = FlaskUI(app, width=800, height=600)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/", methods=['POST'])
+def upload_files():
+    print("in upload_files")
+    print(request)
+    upload_file = request.files['file']
+    print(upload_file)
+    if upload_file.filename != '':
+        print(upload_file.filename)
+        upload_file.save('uploaded_files/' + upload_file.filename)
+    return redirect(url_for('index'))
+
+
 if __name__ == "__main__":
-    ui.run()
+    #app.run()
+    app.run(debug=True)
+    #ui.run()
+    #delete a file after calulation 
+    
